4b88a2bca96b65505871f6fdafd86fa1c750ce9d
Mamata  Ingole <mamataingole125@gmail.com>
2022-08-27 11:43:27 +0530

    team file deleted

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
-
+import ToggleBook from "./Components/prices/ToggleBook"
+import ChooseToggleTime from "./Components/prices/ChooseToggleTime"
+import YourInfo from "./Components/prices/YourInfo"
+import BookingConfirm from "./Components/prices/BookingConfirm"
+import Prices from './Components/prices/Prices';
 
 function App() {
   return (
     <div className="App">
-  
+      {/* <ToggleBook/> */}
+     {/* <ChooseToggleTime/> */}
+    {/* <YourInfo/> */}
+    {/* <BookingConfirm/> */}
+    <Prices/>
     </div>
   );
 }
