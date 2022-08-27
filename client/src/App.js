03f91f01833dac9e03d721c0d821043b23cf80fa
Mamata  Ingole <mamataingole125@gmail.com>
2022-08-27 11:50:27 +0530

    checked

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import Prices from "./Components/prices/Prices"
 function App() {
   return (
     <div className="App">
-     
+    
     </div>
   );
 }
